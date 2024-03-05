1d11f2662f54aae03a4416ed37b9e29f68bb753e
Isak Lindgren <114653175+IsakLindgren@users.noreply.github.com>
2024-03-05 08:59:17 +0100

    added sorting + minor changes

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -15,10 +15,32 @@ namespace SQLiteTest.Controllers
             _dataContext = context;
         }
 
+
+        [HttpPost]
+        public async Task<ActionResult<List<Article>>> AddArticle(Article article)
+        {
+            _dataContext.Articles.Add(article);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.Articles.ToListAsync());
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<Article>>> GetAllArticles()
         {
             return Ok(await _dataContext.Articles.ToListAsync());
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Article>>> GetAllArticles(int id)
+        {
+            var article = await _dataContext.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return BadRequest("article not found.");
+            }
+            return Ok(article);
+        }
+
     }
 }
